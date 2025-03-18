57cab1de186f633895a422d57b8bf6b550c3de67
vijayda1891 <vijayda2009@gmail.com>
2025-03-18 08:49:08 +0530

    Step 3

--- Incubyte_assesment/lib/string_calculator.rb
+++ Incubyte_assesment/lib/string_calculator.rb
@@ -11,12 +11,24 @@ class StringCalculator
         #     sample_string.to_i
         # end
 
-        #step 1 part c
+        #step 1 part c and step 2 
+        # result = 0
+        # if sample_string.length == 0
+        #     result
+        # else
+        #     x = sample_string.split(",").map(&:to_i)
+        #     x.each do |i|
+        #         result += i
+        #     end
+        # end
+        # result
+
+        #step 3
         result = 0
         if sample_string.length == 0
             result
         else
-            x = sample_string.split(",").map(&:to_i)
+            x = sample_string.split(/[,|\n]/).map(&:to_i)
             x.each do |i|
                 result += i
             end
--- Incubyte_assesment/spec/string_calculator_spec.rb
+++ Incubyte_assesment/spec/string_calculator_spec.rb
@@ -15,10 +15,16 @@ describe StringCalculator do
         #     end 
         # end
 
-        context "given string with multiple characters" do
-            it "should return the sum of the characters" do 
-                expect(StringCalculator.add("1,5")).to eq(6)
-            end
+        # context "given string with multiple characters separated by commas" do
+        #     it "should return the sum of the numbers" do 
+        #         expect(StringCalculator.add("1,5")).to eq(6)
+        #     end
+        # end
+
+        context "given string with new lines and commas" do
+            it "should return the sum of the numbers in the string" do 
+                expect(StringCalculator.add("1\n2,3")).to eq(6)
+            end 
         end
     end
 
